5d94a314d0a5cb436fff7a2d8b0685207cc422cc
enas48 <enasm4829@gmail.com>
2020-09-27 17:19:29 +0200

    container - presentational component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -21,7 +21,7 @@ const commentItem =comments.map((comment)=>{
     return(
       <li key={comment.id}>
          <p>{comment.comment}</p>
-         <p>--{comment.author}, {comment.date}</p>
+         <p>--{comment.author}, {new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
       </li>
     )
 })
@@ -41,6 +41,7 @@ return (
     render(){
         if(this.props.dish != null){
             return(
+                <div className="container">
                 <div className="row">
                      <div className="col-12 col-md-5 m-1 ">
                          {this.renderDish(this.props.dish)}
@@ -48,6 +49,7 @@ return (
                      <div className="col-12 col-md-5 m-1 ">
                          {this.renderComments(this.props.dish.comments)}
                      </div>
+                </div>
                 </div>
             )
     
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
 import { Card,CardTitle,CardImg ,CardImgOverlay} from 'reactstrap';
-import DishDetail from './DishdetailComponent';
 
 class Menu extends Component{
-constructor(props){
-    super(props);
-    this.state={
-        selectedDish:null
-    }
-}
-    
-onDishSelected(dish){
-  this.setState({
-      selectedDish:dish
-  })
-
-}
-
-
     render(){
         const menu = this.props.dishes.map((dish)=>{
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={()=> this.onDishSelected(dish)}>
+                    <Card onClick={()=>this.props.onClick(dish.id)}>
                          <CardImg width="100%" src={dish.image} alt={dish.name}/>
         
                         <CardImgOverlay className="ml-5">
@@ -38,7 +22,6 @@ onDishSelected(dish){
              <div className="row">
                    {menu}
              </div>
-             <DishDetail dish={this.state.selectedDish}/>
          </div>
         );
     }
